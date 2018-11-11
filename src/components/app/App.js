7e43eec2db14f5371b1b8ccfcd2255e57fc7dcad
Jared Jones <jkjones.dev@gmail.com>
2018-11-11 09:54:02 -0600

    Setting up base file structure and eslint config

---
+++ src/components/app/App.js
@@ -0,0 +1,19 @@
+// Dependencies
+import React, { Component } from 'react';
+// Styles
+import './App.css';
+
+class App extends Component {
+  console.log('woah');
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Quo</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
