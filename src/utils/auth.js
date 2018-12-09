783ab7ad635db0c5d565a548df8b82a8e449c2d4
Jared Jones <jkjones.dev@gmail.com>
2018-12-08 21:54:59 -0600

    updates

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 
 const config = {
-    apiKey: ' AIzaSyA9uR4dU5OlG2L0rgpbJ_nX_EN1gnIsMuA ',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: 'testproject-d3c33.firebaseapp.com',
 };
 firebase.initializeApp(config);
